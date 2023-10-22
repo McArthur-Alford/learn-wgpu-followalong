ba16788ce48e9cb432938b23869cbce85b9b3146
mcarthur <mcarthur.alford@proton.me>
2023-10-23 00:32:06 +1000

    Sin shader

--- src/tutorial1_surface.rs
+++ src/tutorial1_surface.rs
@@ -1,4 +1,55 @@
+use bytemuck;
+use wgpu::util::DeviceExt;
 use winit::{event::WindowEvent, window::Window};
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-1.0, -1.0, 0.0],
+        color: [139.0, 233.0, 253.0],
+    }, // 0
+    Vertex {
+        position: [-1.0, 1.0, 0.0],
+        color: [80.0, 250.0, 123.0],
+    }, // 1
+    Vertex {
+        position: [1.0, -1.0, 0.0],
+        color: [255.0, 184.0, 108.0],
+    }, // 2
+    Vertex {
+        position: [1.0, 1.0, 0.0],
+        color: [255.0, 121.0, 198.0],
+    }, // 3
+];
+
+const INDICES: &[u16] = &[0, 2, 1, 1, 2, 3];
 
 pub struct State {
     surface: wgpu::Surface,
@@ -8,6 +59,10 @@ pub struct State {
     pub size: winit::dpi::PhysicalSize<u32>,
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -93,7 +148,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[],
+                buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -122,6 +177,22 @@ impl State {
             multiview: None,
         });
 
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let num_indices = INDICES.len() as u32;
+
+        let num_vertices = VERTICES.len() as u32;
+
         Self {
             window,
             surface,
@@ -130,6 +201,10 @@ impl State {
             config,
             size,
             render_pipeline,
+            vertex_buffer,
+            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -175,9 +250,9 @@ impl State {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
+                            r: rgb_to_srgb(40.0).into(),
+                            g: rgb_to_srgb(42.0).into(),
+                            b: rgb_to_srgb(54.0).into(),
                             a: 1.0,
                         }),
                         store: true,
@@ -187,7 +262,9 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
@@ -196,4 +273,8 @@ impl State {
 
         Ok(())
     }
+}
+
+fn rgb_to_srgb(col: f32) -> f32 {
+    ((col / 255.0 + 0.055) / 1.055).powf(2.4)
 }
